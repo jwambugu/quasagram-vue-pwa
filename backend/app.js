e90be5cd0e1fbab01f1e51dabd81c99da1849eb0
Joram Wambugu <artisan254@gmail.com>
2021-05-26 21:54:00 +0300

    Firebase
    Get posts from firebase.

--- backend/app.js
+++ backend/app.js
@@ -4,26 +4,37 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//Firebase db
+const db = require("./firebase");
+
 // Get all posts
 app.get("/posts", (req, res) => {
-  const posts = [
-    {
-      caption: "Golden Gate Bridge",
-      location: "San Francisco",
-    },
-    {
-      caption: "London Eye",
-      location: "London",
-    },
-  ];
+  res.set("Access-Control-Allow-Origin", "*");
 
-  res.send({
-    data: {
-      posts,
+  const posts = [];
+
+  const doc = db.collection("posts").orderBy("createdAt", "desc");
+
+  const unsubscribe = doc.onSnapshot(
+    (snap) => {
+      snap.docs.forEach((doc) => {
+        // Must wait for the server to create the timestamp & send it back
+        // We don't want to edit data until it has done this
+        doc.data().createdAt && posts.push({ ...doc.data(), id: doc.id });
+      });
+
+      res.send({
+        data: {
+          posts,
+        },
+      });
     },
-  });
+    (err) => {
+      console.log(err.message);
+    }
+  );
 });
 
 console.log(`[*] App running on port ${port}`);
 
-app.listen(port);
+app.listen(process.env.PORT || port);
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -2,41 +2,78 @@
   <q-page class="constrain q-pa-md">
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-8">
-        <q-card
-          class="card-post q-mb-md"
-          flat
-          bordered
-          v-for="post in posts"
-          :key="post.id"
-        >
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img
-                  src="https://pbs.twimg.com/profile_images/984725888205053952/06e9L7cz.jpg"
-                  alt="Avatar"
-                />
-              </q-avatar>
-            </q-item-section>
+        <template v-if="!loadingPosts && posts.length">
+          <q-card
+            class="card-post q-mb-md"
+            flat
+            bordered
+            v-for="post in posts"
+            :key="post.id"
+          >
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img
+                    src="https://pbs.twimg.com/profile_images/984725888205053952/06e9L7cz.jpg"
+                    alt="Avatar"
+                  />
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label class="text-bold">jwambugu</q-item-label>
-              <q-item-label caption>{{ post.location }}</q-item-label>
-            </q-item-section>
-          </q-item>
+              <q-item-section>
+                <q-item-label class="text-bold">jwambugu</q-item-label>
+                <q-item-label caption>{{ post.location }}</q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-separator />
+            <q-separator />
 
-          <q-img :src="post.imageUrl" :alt="post.caption" />
+            <q-img :src="post.imageUrl" :alt="post.caption" />
 
-          <q-card-section>
-            <div>{{ post.caption }}</div>
+            <q-card-section>
+              <div>{{ post.caption }}</div>
 
-            <div class="text-caption text-grey">
-              {{ post.createdAt | niceDate }}
-            </div>
-          </q-card-section>
-        </q-card>
+              <div class="text-caption text-grey">
+                {{ post.createdAt | niceDate }}
+              </div>
+            </q-card-section>
+          </q-card>
+        </template>
+
+        <template v-else-if="!loadingPosts && !posts.length">
+          <h5 class="text-center text-grey">No posts yet.</h5>
+        </template>
+
+        <template v-else>
+          <q-card flat bordered v-for="i in 2" class="q-mb-md" :key="i">
+            <q-item>
+              <q-item-section avatar>
+                <q-skeleton type="QAvatar" animation="fade" size="40px" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+                <q-item-label caption>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-skeleton height="200px" square animation="fade" />
+
+            <q-card-section>
+              <q-skeleton type="text" class="text-subtitle2" animation="fade" />
+              <q-skeleton
+                type="text"
+                width="50%"
+                class="text-subtitle2"
+                animation="fade"
+              />
+            </q-card-section>
+          </q-card>
+        </template>
       </div>
 
       <div class="col-4 large-screen-only">
@@ -67,41 +104,40 @@ export default {
   name: "PageHome",
   data() {
     return {
-      posts: [
-        {
-          id: 1,
-          caption: "Golden Gate Bridge",
-          createdAt: 1622010938600,
-          location: "Nairobi, Kenya",
-          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        },
-        {
-          id: 2,
-          caption: "Golden Gate Bridge",
-          createdAt: 1622010938600,
-          location: "Nairobi, Kenya",
-          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        },
-        {
-          id: 3,
-          caption: "Golden Gate Bridge",
-          createdAt: 1622010938600,
-          location: "Nairobi, Kenya",
-          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        },
-        {
-          id: 4,
-          caption: "Golden Gate Bridge",
-          createdAt: 1622010938600,
-          location: "Nairobi, Kenya",
-          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        },
-      ],
+      posts: [],
+      loadingPosts: false,
     };
   },
   filters: {
     niceDate(timeStamp) {
       return date.formatDate(timeStamp, "MMMM D, HH:m");
+    },
+  },
+  created() {
+    this.getPosts();
+  },
+  methods: {
+    getPosts() {
+      this.loadingPosts = true;
+
+      this.$axios
+        .get("http://localhost:3000/posts")
+        .then(({ data }) => {
+          const { posts } = data.data;
+
+          this.posts = posts;
+          this.loadingPosts = false;
+        })
+        .catch((error) => {
+          this.loadingPosts = false;
+
+          console.log(error);
+
+          this.$q.dialog({
+            title: "Error",
+            message: "Could not get posts.",
+          });
+        });
     },
   },
 };
