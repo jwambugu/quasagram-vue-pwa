2b759a72229239483e3084bbd3bb3bfd0c1c771b
Joram Wambugu <artisan254@gmail.com>
2021-05-31 11:14:51 +0300

    PWA: Read keys from the .env

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,11 +1,19 @@
+require("dotenv").config();
+
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+// const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
   storageBucket: "quasagram-e0c85.appspot.com",
 });
+
+console.log(process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"));
 
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
